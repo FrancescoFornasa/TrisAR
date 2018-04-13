fc2b7142c6e4ced6e830f12d7572da4d77505b82
Francesco <fornasa.francesco@gmail.com>
2018-04-13 21:11:18 +0200

    Inizio detection di cerchi in visione.py

--- visione.py
+++ visione.py
@@ -1,9 +1,19 @@
 import cv2
+import numpy as np
 
 
 def osserva_settore(roi):
     # TODO
-    return ' '
+    roi = cv2.cvtColor(roi, cv2.COLOR_RGB2GRAY)
+    cerchi = cv2.HoughCircles(roi, cv2.HOUGH_GRADIENT, 1.1, 1)
+
+    if cerchi is not None:
+        cerchi = np.uint16(np.around(cerchi))
+        cerchi = cerchi[0]
+
+        # for c in cerchi
+
+    return cerchi
 
 
 def guarda_griglia(warped_frame, posizioni_angoli, dimensione_settore):
